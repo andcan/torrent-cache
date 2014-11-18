7d54451cf64e9210516b1246e5230e2b8b92cad2
Andrea Cantafio <kk4r.1m@gmail.com>
2014-11-18 21:26:31 +0100

    Updated setup

--- bin/setup.py
+++ bin/setup.py
@@ -1,13 +1,21 @@
 import os
-from subprocess import call
+import sys
+from subprocess import Popen, PIPE
+
+def call(args) :
+
+    p = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    for line in p.stdout:
+        print(line)
+    return
 
 if not os.path.isfile("cached.tar.gz") :
     call(["tar", "cfzT", "cached.tar.gz", "/dev/null"])
 
 dir = os.getenv("HOME") + "/.torrent_cache"
-if not os.path.isfile(dir) :
+if not os.path.exists(dir) :
     os.makedirs(dir)
 
 call(["godep", "get"])
-call(["go", "build", "torrent_cache.go"])
+call(["go", "build"])
 
