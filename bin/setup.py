f30486711c83dfbb3da2a0737ebd5d972a1aacff
Andrea Cantafio <kk4r.1m@gmail.com>
2014-11-18 20:48:54 +0100

    Automatic compilation

--- bin/setup.py
+++ bin/setup.py
@@ -3,3 +3,11 @@ from subprocess import call
 
 if not os.path.isfile("cached.tar.gz") :
     call(["tar", "cfzT", "cached.tar.gz", "/dev/null"])
+
+dir = os.getenv("HOME") + "/.torrent_cache"
+if not os.path.isfile(dir) :
+    os.makedirs(dir)
+
+call(["godep", "get"])
+call(["go", "build", "torrent_cache.go"])
+
